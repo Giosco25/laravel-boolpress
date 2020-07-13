e07a0316590465010a09174e04ce724c18081a59
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-07-13 16:52:19 +0200

    Post Controller

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>Login</h3>
+            </div>
+
+        </div>
+
+    </div>
+@endsection
